dffe4079fee5c8b15b94faecadccce96813ebd14
Jay Hannes <Hannes_E@gmx.de>
2015-01-06 08:52:12 +0100

    Use middleware to serve static content

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static('public'));
 
 app.get('/parts', function(request, response) {
   response.redirect(301, '/blocks')
